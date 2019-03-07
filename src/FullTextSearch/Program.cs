189ca75dc7e39d30c71e4598b9f4b45041ff7449
wk-j <somnuk.wk@gmail.com>
2019-03-07 17:27:25 +0700

    C# 8

--- src/FullTextSearch/Program.cs
+++ src/FullTextSearch/Program.cs
@@ -17,92 +17,113 @@ namespace FullTextSearch {
             return builder.Options;
         }
 
-        static void Main(string[] args) {
+        static bool Insert() {
             var options = CreateOptions();
+            var faker =
+                new Faker<Student>()
+                    .CustomInstantiator(f => new Student {
+                        //Name = f.Name.FirstName(),
+                        //Profile1 = f.Lorem.Paragraph(),
+                        //Profile2 = f.Lorem.Paragraph(),
+                        //Profile3 = f.Lorem.Paragraph()
+                    })
+                    .RuleFor(s => s.Name, f => f.Name.FirstName())
+                    .RuleFor(s => s.Profile1, f => f.WaffleText(2))
+                    .RuleFor(s => s.Profile2, f => f.WaffleText(2))
+                    .RuleFor(s => s.Profile3, f => f.WaffleText(2))
+                    .FinishWith((f, s) => {
+                        // Console.WriteLine(s.Profile1);
+                    });
 
-            if (args.Contains("--insert")) {
-                var faker =
-                    new Faker<Student>()
-                        .CustomInstantiator(f => new Student {
-                            //Name = f.Name.FirstName(),
-                            //Profile1 = f.Lorem.Paragraph(),
-                            //Profile2 = f.Lorem.Paragraph(),
-                            //Profile3 = f.Lorem.Paragraph()
-                        })
-                        .RuleFor(s => s.Name, f => f.Name.FirstName())
-                        .RuleFor(s => s.Profile1, f => f.WaffleText(2))
-                        .RuleFor(s => s.Profile2, f => f.WaffleText(2))
-                        .RuleFor(s => s.Profile3, f => f.WaffleText(2))
-                        .FinishWith((f, s) => {
-                            // Console.WriteLine(s.Profile1);
-                        });
+            var count = 0;
 
-                var count = 0;
-
-                foreach (var item in Enumerable.Range(0, 10)) {
-                    var items = 1000;
-                    var data = Enumerable.Range(1, items).Select(x => faker.Generate());
-                    using (var context = new MyContext(options)) {
-                        context.Database.EnsureCreated();
-                        context.Students.AddRange(data);
-                        context.SaveChanges();
-                    }
-                    Console.WriteLine(count += items);
-                }
-            }
-
-            if (args.Contains("--count")) {
+            foreach (var item in Enumerable.Range(0, 10)) {
+                var items = 1000;
+                var data = Enumerable.Range(1, items).Select(x => faker.Generate());
                 using (var context = new MyContext(options)) {
-                    var count = context.Students.Count();
-                    Console.WriteLine(count);
+                    context.Database.EnsureCreated();
+                    context.Students.AddRange(data);
+                    context.SaveChanges();
                 }
+                Console.WriteLine(count += items);
             }
 
-            var keywords = "object";
+            return true;
+        }
 
-            if (args.Contains("--contains")) {
-                using (var context = new MyContext(options)) {
-                    var data = context.Students
-                        .Where(x =>
-                            x.Profile1.Contains(keywords) ||
-                            x.Profile2.Contains(keywords) ||
-                            x.Profile3.Contains(keywords)
-                        )
-                        .Select(x => x.Name).ToList();
-                    Console.WriteLine(data.Count());
-                }
+        static bool Count() {
+            var options = CreateOptions();
+            using (var context = new MyContext(options)) {
+                var count = context.Students.Count();
+                Console.WriteLine(count);
             }
+            return true;
+        }
 
-
-            if (args.Contains("--like")) {
-                using (var context = new MyContext(options)) {
-                    var data = context.Students
-                        .Where(x =>
-                            EF.Functions.Like(x.Profile1, "%" + keywords + "%") ||
-                            EF.Functions.Like(x.Profile2, "%" + keywords + "%") ||
-                            EF.Functions.Like(x.Profile3, "%" + keywords + "%")
-                        )
-                        .Select(x => x.Name).ToList();
-                    Console.WriteLine(data.Count());
-                }
+        static bool Contains(string keywords) {
+            var options = CreateOptions();
+            using (var context = new MyContext(options)) {
+                var data = context.Students
+                    .Where(x =>
+                        x.Profile1.Contains(keywords) ||
+                        x.Profile2.Contains(keywords) ||
+                        x.Profile3.Contains(keywords)
+                    )
+                    .Select(x => x.Name).ToList();
+                Console.WriteLine(data.Count());
             }
+            return true;
+        }
+
+        static bool Like(string keywords) {
+            var options = CreateOptions();
+            using (var context = new MyContext(options)) {
+                var data = context.Students
+                    .Where(x =>
+                        EF.Functions.Like(x.Profile1, "%" + keywords + "%") ||
+                        EF.Functions.Like(x.Profile2, "%" + keywords + "%") ||
+                        EF.Functions.Like(x.Profile3, "%" + keywords + "%")
+                    )
+                    .Select(x => x.Name).ToList();
+                Console.WriteLine(data.Count());
+            }
+            return true;
+        }
+
+        static bool FreeText(string keywords) {
 
             /*
             DROP INDEX fts_idx
             CREATE INDEX fts_idx ON public."Students" USING GIN (to_tsvector('english', "Profile1" || ' ' || "Profile2" || ' ' || "Profile3" ))
             */
 
-            if (args.Contains("--freetext")) {
-                using (var context = new MyContext(options)) {
-                    var data = context.Students
-                        .Where(x =>
-                                EF.Functions
-                                    .ToTsVector("english", x.Profile1 + " " + x.Profile2 + " " + x.Profile3)
-                                    .Matches(keywords)
-                        ).Select(x => x.Name).ToList();
-                    Console.WriteLine(data.Count);
-                }
+            var options = CreateOptions();
+            using (var context = new MyContext(options)) {
+                var data = context.Students
+                    .Where(x =>
+                            EF.Functions
+                                .ToTsVector("english", x.Profile1 + " " + x.Profile2 + " " + x.Profile3)
+                                .Matches(keywords)
+                    ).Select(x => x.Name).ToList();
+                Console.WriteLine(data.Count);
             }
+            return true;
+        }
+
+        static void Main(string[] args) {
+            if (args.Length == 0) return;
+
+            var keywords = "object";
+
+            var rs = args[0] switch
+            {
+                "--insert" => Insert(),
+                "--count" => Count(),
+                "--like" => Like(keywords),
+                "--contains" => Contains(keywords),
+                "--freetext" => FreeText(keywords),
+                _ => true
+            };
         }
     }
 }

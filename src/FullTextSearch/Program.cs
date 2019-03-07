312a41c24c23716d4671f4710e71947222068456
wk-j <somnuk.wk@gmail.com>
2019-03-07 14:38:28 +0700

    Initial commit

---
+++ src/FullTextSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FullTextSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
